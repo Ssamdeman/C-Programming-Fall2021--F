4b3862fb2d1c06ad0bfcebb28235f9a017259008
ssam135 <sultan.samid@gmail.com>
2021-10-10 21:01:11 -0400

    worked a little on project and part of coding daily is here too.

---
+++ Hw2/CodingExercises.c
@@ -0,0 +1,26 @@
+
+
+/* int a, b;
+    scanf("%d\n%d", &a, &b);
+    int tempholder;
+    
+    for(tempholder = a; tempholder <= 9;tempholder++){
+        if (tempholder == 1) printf("one\n");
+        if (tempholder == 2) printf("two\n");
+        if (tempholder == 3) printf("three\n");
+        if (tempholder == 4) printf("four\n");
+        if (tempholder == 5) printf("five\n");
+        if (tempholder == 6) printf("six\n");
+        if (tempholder == 7) printf("seven\n");
+        if (tempholder == 8) printf("eight\n");
+        if (tempholder == 9) printf("nine\n");
+    }
+    if( a <= 9 ||a % 2 == 0 ) printf("evenI am here\n ");
+    
+    if (a <= 9 ||a % 2 != 0) printf("odd\n");
+    
+    if (b >= 9 ||b % 2 == 0) printf("even\n");
+    if (b >= 9 ||b % 2 != 0) printf("oddI am here\n ");
+
+        
+    return 0; */
---
+++ Hw2/hw2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define MAXVALUE 1000
+
+int main (void){
+    ///10-10-2021 19:50:25 get the the input from the stdin
+    // Get the first two input.
+    char eachCharacter;
+    char userInputes[MAXVALUE];
+
+    while ((eachCharacter = getchar()) != '\n'){
+        
+
+    }
+
+
+
+
+
+    int a ; /// First  arary but to complete this I need the input numbes.
+    int b ; /// Second Array.
+
+    
+
+
+}
