656bc8b940f705c1aca1fc41eac7bfa9d4daf5d6
Ssam135 <36642632+ssam135@users.noreply.github.com>
2021-10-12 14:02:36 -0400

    Update CodingExercises.c

--- Hw2/CodingExercises.c
+++ Hw2/CodingExercises.c
@@ -1,26 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 
-/* int a, b;
+
+int main() 
+{
+    int a, b, tempholder, valueChecker ;
     scanf("%d\n%d", &a, &b);
-    int tempholder;
-    
-    for(tempholder = a; tempholder <= 9;tempholder++){
-        if (tempholder == 1) printf("one\n");
-        if (tempholder == 2) printf("two\n");
-        if (tempholder == 3) printf("three\n");
-        if (tempholder == 4) printf("four\n");
-        if (tempholder == 5) printf("five\n");
-        if (tempholder == 6) printf("six\n");
-        if (tempholder == 7) printf("seven\n");
-        if (tempholder == 8) printf("eight\n");
-        if (tempholder == 9) printf("nine\n");
+    for(tempholder = a; tempholder <= b; tempholder++){
+     if (tempholder == 1) printf("one\n");
+     if (tempholder == 2) printf("two\n");
+     if (tempholder == 3) printf("three\n");
+     if (tempholder == 4) printf("four\n");
+     if (tempholder == 5) printf("five\n");
+     if (tempholder == 6) printf("six\n");
+     if (tempholder == 7) printf("seven\n");
+     if (tempholder == 8) printf("eight\n");
+     if (tempholder == 9) printf("nine\n");
     }
-    if( a <= 9 ||a % 2 == 0 ) printf("evenI am here\n ");
+    for (valueChecker = a;valueChecker <= b; valueChecker++ ) {
+     if( valueChecker <= 9 && valueChecker % 2 == 0 ) printf("even\n");
     
-    if (a <= 9 ||a % 2 != 0) printf("odd\n");
-    
-    if (b >= 9 ||b % 2 == 0) printf("even\n");
-    if (b >= 9 ||b % 2 != 0) printf("oddI am here\n ");
 
-        
-    return 0; */
+     if (valueChecker >= 9 && valueChecker % 2 != 0) printf("odd\n");
+    }
+    return 0;
+}
+
