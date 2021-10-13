15bd55ab33e9fcc1f3e599a2848bf780b08d67f6
ssam135 <sultan.samid@gmail.com>
2021-10-13 11:10:41 -0400

    last changes from my pc

--- Hw2/hw2.c
+++ Hw2/hw2.c
@@ -5,11 +5,14 @@
 int main (void){
     ///10-10-2021 19:50:25 get the the input from the stdin
     // Get the first two input.
+    //10-12-2021 22:41:22 the big issue is that the Pc is functioning differently and then my MAC. yea so using this PC for now is useless. 
+    //10-12-2021 23:02:09 I have used from stackoverflow. Let's see what they will say. 
     char eachCharacter;
     char userInputes[MAXVALUE];
     int i; // tracker for user inputes
-     printf("I am here \n");
-    if ((eachCharacter = getchar()) == '\n') {
+    eachCharacter = getchar();
+
+    if (eachCharacter == '\n') {
         printf("%c",eachCharacter );
         userInputes[1] = '\0';
         printf("I am here");
@@ -17,15 +20,11 @@ int main (void){
     else{
         while(eachCharacter != EOF){
             userInputes[i] = eachCharacter;
-            ++i;
+            i++;
             eachCharacter = getchar(); 
         }
     userInputes[i] = '\0';
     }
-    printf("I am here %s \n", userInputes); 
-
-
-    
-
+    printf("I am here %s \n", userInputes);
 
 }
