e97c98ae30874b66304a4affc852fb84dfed281c
ssam135 <sultan.samid@gmail.com>
2021-10-16 01:50:49 -0400

    hw2 done by 80 percent

--- Hw2/hw2Part2.c
+++ Hw2/hw2Part2.c
@@ -9,6 +9,7 @@ int main (void){
     int n = 0; /// length of the array A;
     int m = 0; // length fo the array B;
 
+
     int x; // main savefor the for input
     int t = 0; // for array 3
     /// pointers for all the arrays;
@@ -16,7 +17,16 @@ int main (void){
     int* pttwo;
     int* ptthree;
 
-    scanf("%d %d", &n, &m);
+    scanf("%d", & n);
+    scanf("%d", & m);
+
+
+   
+
+    if(n == 0 && m ==0) {
+        printf("\n");
+      
+    }
 
     int arrayA [n];
     int arrayB [m];
@@ -28,47 +38,65 @@ int main (void){
     
     for(int i = 0; i < n; i++){
         scanf("%d,", &x);
-        arrayA[i] = x;
+        *(ptone + i) = x;
     }
     
     for(int k = 0; k < m; k++){
         scanf("%d,", &x);
-        arrayB[k] = x;
+        *(pttwo + k) = x;
     }
+
     /* for(int w = 0; w < n; w++){
         printf("%d,", arrayA[w]);
     }
     for(int w = 0; w < m; w++){
         printf("%d,", arrayB[w]);
     } */
+
+    for(int y = 0; y < n; y++){
+        *(ptthree + t) = *(ptone + y);
+        t++;
+    }
+    for(int y = 0; y < m; y++){
+        *(ptthree + t) = *(pttwo + y);
+        t++;
+    }
+
+    int temp;
+     for (int i = 0; i < n+m; i++){
+        for (int j = 0; j < ((n+m) - i - 1); j++){
+            if (arrayC[j] > arrayC[j + 1]){
+                temp = arrayC[j];
+                arrayC[j] = arrayC[j + 1];
+                arrayC[j + 1] = temp;
+            }
+        }
+    }
+
+
+
+
+   /*  for(int r = 0; r <(n+m); r++){
+        if(arrayC[r] <= arrayC[r+1] )
+            arrayC[r] = arrayC[r];
+        else{
+            arrayC[r+1] =   
+        }
+    } */
+    
     
     // this function is not working
-    int q;
-    int p;
-    while (q < n && p < m) {
-        if (arrayA[q] < arrayB[p]) {
-            *(ptthree + t) = *(ptone + q);
-            t++, q++;
-        }
-        else {
-            *(ptthree + t) = *(pttwo + p);
-            t++, p++;
-        }
-    }
-     for(int w = 0; w < m+n ; w++){
-        printf("%d,\n", arrayC[w]);
-    }
-  
 
-   /* int tempHolder;
+
+   int tempHolder;
     for (tempHolder = 0; tempHolder < n + m - 1; tempHolder++) {
         printf("%d,", *(ptthree + tempHolder));
     }
     if (tempHolder != 0) {
         printf("%d", *(ptthree + tempHolder));
-    } */
+    }
 
-    //for (int h = 0; h < (n+m) ; h++) printf("%d,", arrayC[h]);
+    
     return 0;
 
 
