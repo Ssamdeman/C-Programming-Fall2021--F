c20ef08bd2ea73d95365255a2218da8b259c20e4
ssam135 <sultan.samid@gmail.com>
2021-10-24 13:00:08 -0400

    Working/learning

--- Every_Concept_from_C/Chapter_1_2.c
+++ Every_Concept_from_C/Chapter_1_2.c
@@ -1,8 +1,21 @@
 #include<stdio.h>
 
-
+void exhcangeValue(int* a, int* b){
+    int tempValue;
+    tempValue = *a;
+    *a = *b;
+    *b = tempValue;
+    
+}
 
 int main (void){
-    
+    char c;
+
+    int x = 10;
+    int y = 15;
+    //we want to change the x and y values respctivally 
+    printf("Before: %d %d\n", x,y);
+    exhcangeValue(&x, &y);
+    printf("After %d %d\n", x, y );
 
 }
