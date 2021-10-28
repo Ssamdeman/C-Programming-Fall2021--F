6bcb5bc3e8a85014dfc6fcc979626f69b100b680
ssam135 <sultan.samid@gmail.com>
2021-10-28 12:23:38 -0400

    worked on class

---
+++ Week8/October28_2021.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(void){
+    struct point
+    {
+        int x;
+        int y;
+    };
+    
+    struct point q = {1,2};
+
+    printf("%d", q.x);
+    
+
+
+
+
+
+
+  /*   char* p = NULL;
+    printf("1; value of p = %lu\n", (unsigned long) p);
+    p = (char*) malloc(10 * sizeof(char));
+    printf("2; value of p = %lu\n", (unsigned long) p);
+    free(p);
+    p = NULL;
+    //get value array [3][2] = 10;
+    p* = (char*) malloc(15*(sizeof(char)));
+    *(p+ i*j+j)
+    printf("3; value of p = %lu\n", (unsigned long) p);
+ */
+
+    return 0;
+
+
+
+
+
+
+}
