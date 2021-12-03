16865f992af93be16bf4aab1cd8a16f09670d149
ssam135 <sultan.samid@gmail.com>
2021-12-03 08:41:22 -0500

    hw4.

--- HW4/hw4.c
+++ HW4/hw4.c
@@ -6,7 +6,7 @@
 struct NODE {
     int character; // character stored in this node
     struct NODE *next; // address pointing to the next node in the list
-    struct NODE *prev; 
+    struct NODE *prev; //address of prev nodes.
 };
 
 
@@ -15,10 +15,20 @@ struct NODE {
 void printList(struct NODE *first) {
     // print information of linked list starting at the first node "first"
     struct NODE *current;
+    struct NODE *currentPrev;
     current = first;
+    currentPrev = NULL;
     while(current != NULL) {
-        printf("%d ", (*current).character);
-        current = (*current).next;
+        printf("%d ", current->character);
+        currentPrev = current;
+        current = current->next;
+    }
+    printf("\n");
+    current = currentPrev; 
+    while(current != NULL){
+        printf("%d ", current->character);
+        current = current->prev;
+
     }
 }
 
@@ -41,8 +51,27 @@ void printList(struct NODE *first) {
         return w;
     } */
 
-void ReversePrint(struct NODE *list) {
-	struct NODE* temp = list;
+/* void ReversePrint(struct NODE *p1) {
+    if (p1 == NULL) return;
+    struct NODE* last;
+    last = p1;
+    
+    while ((*last).next != NULL) {
+        last = (*last).next;
+    }
+    // last: last mode in list
+    // print all nodes form last backward
+    
+    struct NODE* ptr = last;
+    while (ptr!=NULL) {
+        printf("%d ", (*ptr).character);
+        ptr = (*ptr).prev;
+    }
+    
+
+ */
+
+	/* struct NODE* temp = list;
 	if(temp == NULL) return; // empty list, exit
 	// Going to last Node
 	while(temp->next != NULL) {
@@ -52,12 +81,11 @@ void ReversePrint(struct NODE *list) {
 		printf("%d ",temp->character);
 		temp = temp->prev;
 	}
-	printf("\n");
-} 
+	printf("\n"); */
 
 
-struct NODE* AddNewNodeSorted(struct NODE *first, int ch){
-    struct NODE *ptr;
+/* struct NODE* AddNewNodeSorted(struct NODE *first, int ch){
+    
 	struct NODE *temp = (struct NODE *)malloc(sizeof(struct NODE));
 	temp->character = ch;
 	temp->next = NULL;
@@ -65,34 +93,38 @@ struct NODE* AddNewNodeSorted(struct NODE *first, int ch){
 	
 	if(first == NULL){
 		// this section runs if linked list is empty
-		// we simply set head to temp
 		first = temp;	
 	}
+
+    //check if if the it's value equal to it self.
 	else if(temp->character <= first->character){
 		temp->next = first;
+        first->prev= first; // if first and temp is 1 then assing first one to temp 1.
 		first = temp;
+        //I dont know what to do with previus value in case of fist has 1 and 1. I made that in that case, I just
+        first->prev = NULL; //this one does not make sense so. later
 	}
 	else{
-		ptr = first;
+	    struct NODE *ptr = first;
 		while(ptr->next != NULL && ptr->next->character < temp->character){
 			ptr = ptr->next;
 		}
 		temp->next = ptr->next;
-        //temp->prev = ptr;
+        temp->prev = ptr;
 		ptr->next = temp;
 	}
-	
 	return first;
 
-}
+} */
 
 
 struct NODE* addNewNode(struct NODE *first, int ch) {
     // add a new node "new" to the list starting at address "first"
     // return the updated list
+    
+
     //this code works too but it breaks the reverse.
-    /* 
- */
+
 
     struct NODE *new_node;
     new_node = malloc(sizeof(struct NODE));
@@ -114,7 +146,7 @@ struct NODE* addNewNode(struct NODE *first, int ch) {
         }
         last->next = new_node;
         new_node->prev = last;
-        last = new_node;
+        //last = new_node;
     }
     return first;
 }
@@ -126,22 +158,20 @@ int main() {
 
     scanf ("%d ", &y);
     struct NODE *p = NULL; //reversiable
-    struct NODE *p_two = NULL; //sorted but none reversiable. 
-    struct NODE *p_three = NULL;
+    //struct NODE *p_two = NULL; //sorted but none reversiable. 
+    //struct NODE *p_three = NULL;
 
     for (int i = 0; i < y; i++){
         scanf("%d", &x);
+        //p = addNewNode(p, x);
+        //p_two = AddNewNodeSorted(p_two, x);
         p = addNewNode(p, x);
-        p_two = AddNewNodeSorted(p_two, x);
     }
     scanf ("%d", &z);
+    p = addNewNode(p, z);
 
-    printf("the string just read from stdin is: \n");
-
-    p_two = AddNewNodeSorted(p_two, z);
+    printf("Priting linked List BEFORE reverse it.\n");
     printList(p);
-    printf("\n");
-    ReversePrint(p_two);
     
     return 0;
 }
@@ -184,3 +214,47 @@ struct node* insert_sortedll(node *head, int val){
 	
 	return head;
  */
+
+/* 12-02-2021 12:15:12 All the inpute 
+Input1
+5 
+1 3 5 7 9 
+2
+Output1
+1 2 3 5 7 9 
+9 7 5 3 2 1
+
+
+Input2
+5
+1 3 5 7 9 
+10
+Output2
+1 3 5 7 9 10
+10 9 7 5 3 1
+Input3
+5 
+1 3 5 7 9      
+0
+Output3
+0 1 3 5 7 9
+9 7 5 3 1 0
+Input4
+0
+10
+Output4
+10
+10
+Input5
+1
+10
+10 
+Output5: 
+10 10 
+10 10
+
+
+
+
+
+*/
