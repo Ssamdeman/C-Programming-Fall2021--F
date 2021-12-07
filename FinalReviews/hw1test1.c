304a5ab8b0e679225e25a2ea23a32da5fa4d0c0e
ssam135 <sultan.samid@gmail.com>
2021-12-07 13:39:11 -0500

    hw1 is done

--- FinalReviews/hw1test1.c
+++ FinalReviews/hw1test1.c
@@ -2,6 +2,7 @@
 #define MAXLENGTH 1000
 //12-07-2021 11:04:11 Classes note: Focus on the Test3 and Test 2. 
 // play with linked list a lot. Like a lote. taking and remover node
+//12-07-2021 12:48:23 Done.
 int main (void){
 
     int array1 [MAXLENGTH]; // array to save
@@ -16,23 +17,29 @@ int main (void){
     }
     array1[k] = '\0';
 
-    //get the last element.
+    //get the last element.--d
     //printf("\n%c\n",array1[k-1]);
 
     
-    //if(array1[k-1]) > 'a' && array1[k-1]
+    // based on ASCII table
+    if( array1[k-1] > 'a' && array1[k-1] < 'z'){
+        array1[k-1] = array1[k-1] - 32;
+    }
     
 
 
-
+/* inputs
+    123
+    aB
+    
+    c 
+    */
 
 //testing the inputes;
-/*     for ( int i = 0; i < k; i++){
+
+    for ( int i = 0; i < k; i++){
         printf("%c", array1[i] );
-    } */
-
-
-
+    }
 
 
 }
