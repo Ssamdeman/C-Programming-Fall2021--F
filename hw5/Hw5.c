ad1158933ec6dd59e647ff2db45be33b47e8949c
ssam135 <sultan.samid@gmail.com>
2021-12-09 13:39:44 -0500

    hw5

--- hw5/Hw5.c
+++ hw5/Hw5.c
@@ -1,15 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 #define MAXLENGTH 1000
 
 int main (void){
-    //how to get text from stdio.
-    char* ch;
-    int c = 1; //counter;
-    ch = (char*) malloc( MAXLENGTH * ((sizeof(char)+1)));
-    
-    scanf("%s ",ch);
-    printf("%s\n", ch);
+    /* -------------------------------------------------------------------------- */
+                /* CODE FROM INTERNET*/
+    //char *fgets(char *str, int n, FILE *stream)
+    //Might be another option
+     char* c;
+    c = (char*) malloc( MAXLENGTH * ((sizeof(char)+1)));
+    fgets(c, MAXLENGTH, stdin);
+    printf("%s\n", c);
+     
 
+
+    /* -------------------------------------------------------------------------- */
+
+
+
+
+    //how to get text from stdio.
+    //i learned how to do it. 
+    char ch;
+    char* arry_word;
+    int counter = 1; //counter;
+    arry_word = (char*) malloc( MAXLENGTH * ((sizeof(char)+1)));
+    scanf("%s", &ch);
+    while (ch != EOF) {
+        printf("%c\n", ch);
+    }
+    
+
+    //Binary Tree. takes in Sorted. 
+        //1) first the inpute and then put in the binery tree. https://hackr.io/blog/binary-search-in-c
 }
