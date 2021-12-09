70131a0bbc06432e320c0ee89fdcb05cc2c2b29b
ssam135 <sultan.samid@gmail.com>
2021-12-09 12:56:17 -0500

    hw5 worked

--- hw5/Hw5.c
+++ hw5/Hw5.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+#define MAXLENGTH 1000
 
 int main (void){
+    //how to get text from stdio.
+    char* ch;
+    int c = 1; //counter;
+    ch = (char*) malloc( MAXLENGTH * ((sizeof(char)+1)));
     
+    scanf("%s ",ch);
+    printf("%s\n", ch);
 
 }
