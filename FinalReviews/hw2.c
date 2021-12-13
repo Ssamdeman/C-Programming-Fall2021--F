362c5f0c89f813ac35a508c52a25b1485ca16955
ssam135 <sultan.samid@gmail.com>
2021-12-13 10:44:12 -0500

    hw2

--- FinalReviews/hw2.c
+++ FinalReviews/hw2.c
@@ -10,46 +10,58 @@ int main(void){
     int* p;
 
    //printf("Code is Working\n");
-    scanf("%d %d", &n, &m);
+    scanf("%d %d\n", &n, &m);
    
     if( n < 0 || m < 0) return 0;
     //printf("%d %d", n, m);
 
-    int array_a[n];
-    int array_b[m];
-    int array_c[n+m];
+    char array_a[n];// arrays must be char and not int. That is why the problem is here.
+    char array_b[m];
+    char array_c[n+m];
 
     char value_holder;
-    int k = 0;// counter
+    int k = 0;// counter for Array_a;
+    int j = 0; // counter for Array_b; 
 
-    //reading the first line. T
-   
 
+    //reading the first line. ---Don
     value_holder = getchar();
     while(value_holder != EOF){
-        printf("I am HERE \n");
-        if(value_holder =='\n') break;
+        if(value_holder == '\n') break;
         array_a[k] = value_holder;
         k++;
-        value_holder =  getchar();
-        printf("I am HERE 2 \n");
+        value_holder = getchar();
     }
     array_a[k] = '\0';
-   
+    
+    //testing the inputs if Array_A. 
+    /* for(int i = 0; i < k; i++){
+        printf("%c\n", array_a[i]);
+    } */
+
 
     //Reading the second Line. 
-    k = 0;
-
-   /*  value_holder = getchar();
-    while(value_holder != EOF && value_holder != '\n'){
-        array_b[k] = value_holder;
-        k++;
+    value_holder = getchar();
+    while(value_holder != EOF){
+        if (value_holder == '\n') break;
+        array_b[j] = value_holder;
+        j++;
         value_holder = getchar();
-    }    */
+    }  
+    array_b[j] = '\0';
+    /* testing the inputs if Array_A. 
+    for(int i = 0; i < j; i++){
+        printf("%c\n", array_b[i]);
+    }
+ */
+
+
+
 /* 
     2 3 
     1,3
-    2,4,6 */
+    2,4,6 
+    */
 
 
 
